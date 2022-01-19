3cb541e4db0218fd70f1bbf30ac1364e9638788c
WendSant <wendsonsantanajps@gmail.com>
2022-01-19 02:39:00 -0300

    Criado medicos no painel admin django

--- medico/models.py
+++ medico/models.py
@@ -1,3 +1,14 @@
+from tabnanny import verbose
+from this import d
 from django.db import models
 
 # Create your models here.
+
+class Medicos(models.Model):
+   nome = models.CharField(max_length= 50) 
+   email = models.CharField(max_length= 100)
+   senha = models.CharField(max_length=100)
+   data_criacao = models.DateTimeField()
+
+   class Meta:
+       verbose_name = "Medico"
