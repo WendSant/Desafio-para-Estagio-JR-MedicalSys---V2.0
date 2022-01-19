989927d30e30b4d5131001078da4c1b97ff3b239
WendSant <wendsonsantanajps@gmail.com>
2022-01-19 19:50:34 -0300

    Teste de exibir nome user

--- paciente/views.py
+++ paciente/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import redirect, render
 from django.http import HttpResponse
+from medico.models import Medicos
 
 
 def home(request):
     if request.session.get('medico'):
-        return HttpResponse('Hiii')
+        medico = Medicos.objects.get(id=request.session['medico']).nome
+        return HttpResponse(f'Hiii {medico}')
     else:
         return redirect('/auth/login/?status=2')
