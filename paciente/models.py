ce65da3d848246ae621faf6be800172bdcd8b253
WendSant <wendsonsantanajps@gmail.com>
2022-01-19 18:38:04 -0300

    Mudanças app paciente

--- paciente/models.py
+++ paciente/models.py
@@ -1,3 +1,16 @@
+from datetime import date
 from django.db import models
 
-# Create your models here.
+
+class Pacientes(models.Model):
+    nome = models.CharField(max_length=100)
+    telefone = models.CharField(max_length=20)
+    endereco = models.CharField(max_length=100)
+    estado = models.CharField(max_length=20)
+    cidade = models.CharField(max_length=20)
+    territorio = models.CharField(max_length=20)
+    cep = models.CharField(max_length=20)
+    data_criacao = models.DateField(default=date.today)
+
+    class Meta:
+        verbose_name = "Paciente"
