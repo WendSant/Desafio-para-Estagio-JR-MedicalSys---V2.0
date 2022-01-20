afca2ee1166d5deab33c2cc64a66af5f685943e6
WendSant <wendsonsantanajps@gmail.com>
2022-01-20 13:56:36 -0300

    Botão sair feito/funcionando

--- paciente/views.py
+++ paciente/views.py
@@ -8,7 +8,7 @@ def home(request):
     if request.session.get('medico'):
         medico = Medicos.objects.get(id=request.session['medico'])
         pacientes = Pacientes.objects.filter(medico=medico)
-        return render(request, 'home.html', {'pacientes': pacientes})
+        return render(request, 'home.html', {'pacientes': pacientes, 'medico_logado': request.session.get('medico')})
     else:
         return redirect('/auth/login/?status=2')
 
@@ -18,7 +18,7 @@ def ver_pacientes(request, id):
         pacientes = Pacientes.objects.get(id=id)
         if request.session.get('medico') == pacientes.medico.id:
             medico_atual = Medicos.objects.all()
-            return render(request, 'ver_paciente.html', {'paciente': pacientes, 'medico_atual': medico_atual})
+            return render(request, 'ver_paciente.html', {'paciente': pacientes, 'medico_atual': medico_atual, 'medico_logado': request.session.get('medico')})
         else:
             return HttpResponse('Paciente não pertence a este medico')
     return redirect('/auth/login/?status=2')
