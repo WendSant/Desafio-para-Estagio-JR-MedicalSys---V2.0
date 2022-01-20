4f7a121c2d48643e0150d23ab137b17a5254e104
WendSant <wendsonsantanajps@gmail.com>
2022-01-19 23:17:08 -0300

    Listagem de pacientes para cada médico

--- paciente/models.py
+++ paciente/models.py
@@ -1,5 +1,8 @@
 from datetime import date
+from pyexpat import model
 from django.db import models
+
+from medico.models import Medicos
 
 
 class Pacientes(models.Model):
@@ -11,6 +14,10 @@ class Pacientes(models.Model):
     territorio = models.CharField(max_length=20)
     cep = models.CharField(max_length=20)
     data_criacao = models.DateField(default=date.today)
+    medico = models.ForeignKey(Medicos, on_delete=models.DO_NOTHING)
 
     class Meta:
         verbose_name = "Paciente"
+
+    def __str__(self) -> str:
+        return self.nome
--- paciente/views.py
+++ paciente/views.py
@@ -1,11 +1,19 @@
 from django.shortcuts import redirect, render
 from django.http import HttpResponse
 from medico.models import Medicos
+from .models import Medicos, Pacientes
 
 
 def home(request):
     if request.session.get('medico'):
-        medico = Medicos.objects.get(id=request.session['medico']).nome
-        return HttpResponse(f'Hiii {medico}')
+        medico = Medicos.objects.get(id=request.session['medico'])
+        pacientes = Pacientes.objects.filter(medico=medico)
+        return render(request, 'home.html', {'pacientes': pacientes})
     else:
         return redirect('/auth/login/?status=2')
+
+
+def ver_pacientes(request, id):
+    pacientes = Pacientes.objects.get(id=id)
+    print(pacientes)
+    return render(request, 'ver_paciente.html', {'paciente': pacientes})
