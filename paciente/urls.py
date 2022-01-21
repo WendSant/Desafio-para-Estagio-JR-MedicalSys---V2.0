061c9cbd5b6eda57aa12c8addff5e3b68396db93
WendSant <wendsonsantanajps@gmail.com>
2022-01-21 18:59:40 -0300

    Edição paciente funcionando

--- paciente/urls.py
+++ paciente/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     path('ver_paciente/<int:id>', views.ver_pacientes, name="ver_pacientes"),
     path('cadastrar_paciente', views.cadastrar_paciente, name='cadastrar_paciente'),
     path('excluir_paciente/<int:id>',
-         views.excluir_paciente, name="excluir_paciente")
+         views.excluir_paciente, name="excluir_paciente"),
+    path('alterar_paciente', views.alterar_paciente, name="alterar_paciente")
 ]
--- paciente/views.py
+++ paciente/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import redirect, render
+from django.shortcuts import redirect, render, get_object_or_404
 from django.http import HttpResponse
 from medico.models import Medicos
 from .models import Medicos, Pacientes
@@ -58,3 +58,28 @@ def excluir_paciente(request, id):
             return redirect('/paciente/home/?cadastro_paciente=2')
     else:
         return redirect('/auth/login/?status=2')
+
+
+def alterar_paciente(request):
+    paciente_id = request.POST.get('paciente_id')
+    paciente_nome = request.POST.get('paciente_nome')
+    paciente_telefone = request.POST.get('paciente_telefone')
+    paciente_cep = request.POST.get('paciente_cep')
+    paciente_endereco = request.POST.get('paciente_endereco')
+    paciente_estado = request.POST.get('paciente_estado')
+    paciente_cidade = request.POST.get('paciente_cidade')
+    paciente_territorio = request.POST.get('paciente_territorio')
+    paciente = get_object_or_404(Pacientes, id=paciente_id)
+
+    if paciente.medico.id == request.session['medico']:
+        paciente.nome = paciente_nome
+        paciente.telefone = paciente_telefone
+        paciente.cep = paciente_cep
+        paciente.endereco = paciente_endereco
+        paciente.estado = paciente_estado
+        paciente.cidade = paciente_cidade
+        paciente.territorio = paciente_territorio
+        paciente.save()
+        return redirect(f'/paciente/ver_paciente/{paciente_id}')
+    else:
+        return redirect('/auth/sair')
