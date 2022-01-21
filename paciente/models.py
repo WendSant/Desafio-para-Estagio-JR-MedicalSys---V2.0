728cb68fad2ee1437bff5bc0cdb14a516c87811d
WendSant <wendsonsantanajps@gmail.com>
2022-01-21 16:06:56 -0300

    Endereço com autocomplete via cep

--- paciente/models.py
+++ paciente/models.py
@@ -8,11 +8,11 @@ from medico.models import Medicos
 class Pacientes(models.Model):
     nome = models.CharField(max_length=100)
     telefone = models.CharField(max_length=20)
-    endereco = models.CharField(max_length=100)
-    estado = models.CharField(max_length=20)
+    cep = models.CharField(max_length=10)
+    endereco = models.CharField(max_length=50)
+    estado = models.CharField(max_length=2)
     cidade = models.CharField(max_length=20)
-    territorio = models.CharField(max_length=20)
-    cep = models.CharField(max_length=20)
+    territorio = models.CharField(max_length=20, default="Brasil")
     data_criacao = models.DateField(default=date.today)
     medico = models.ForeignKey(Medicos, on_delete=models.DO_NOTHING)
 
