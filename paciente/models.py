d2da613529a0694df8e9e76b9d87cdfc7f59dcf4
WendSant <wendsonsantanajps@gmail.com>
2022-01-21 21:01:47 -0300

    Testando gitignore

--- paciente/models.py
+++ paciente/models.py
@@ -1,14 +1,18 @@
 from datetime import date
 from pyexpat import model
 from django.db import models
-
+from django.core.validators import RegexValidator
 from medico.models import Medicos
+
+my_validator = RegexValidator(
+    "\d{6}\-\d{2}", "CAPA format needs to be ######-##.")
 
 
 class Pacientes(models.Model):
     nome = models.CharField(max_length=100)
     telefone = models.CharField(max_length=20)
-    cep = models.CharField(max_length=10)
+    cep = models.CharField(
+        max_length=10, validators=[my_validator])
     endereco = models.CharField(max_length=50)
     numero = models.CharField(max_length=10)
     estado = models.CharField(max_length=2)
